f53c124f263aecd1cca2b47002807a084cc89ec8
Khaleb Groce <khalebgroce@gmail.com>
2019-01-19 20:37:16 -0600

    CS310

--- Class1.java
+++ Class1.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author Khaleb Groce
  */
 
 public class Class1 {
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author Khaleb Groce
  */
 
 public class HelloWorld {
